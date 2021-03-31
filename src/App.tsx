765aec04e49c52a0fb00af2e7e428789d2f723a4
yuto.yamamura <yamamu24@gmail.com>
2021-03-31 22:54:26 +0900

    募集カードを作成 実装

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import "./App.scss";
 import { BrowserRouter, Route } from "react-router-dom";
 import RecruitmentIndexPage from "./scenes/recruitment/RecruitmentIndexPage";
 import SignInPage from "./scenes/signIn";
+import RecruitmentCard from "./components/RecruitmentCard";
 
 const App: FC = () => (
   <BrowserRouter>
     <div>
       <Route exact path="/" component={RecruitmentIndexPage} />
       <Route path="/sign_in" component={SignInPage} />
+
+      <RecruitmentCard></RecruitmentCard>
     </div>
   </BrowserRouter>
 );
--- src/components/RecruitmentCard/index.tsx
+++ src/components/RecruitmentCard/index.tsx
@@ -1,8 +1,23 @@
 import React from "react";
 import styles from "./style.module.scss";
+import logo from "../../images/EnterpriseLogo/logo01.jpg";
+import avator from "../../images/Avator/M.png";
 
 const RecruitmentCard = () => {
-  return <div className={styles.card}>recruitmentCard</div>;
+  return (
+    <div className={styles.card}>
+      <img className={styles.card_logoImg} src={logo} alt="企業ロゴ" />
+
+      <div className={styles.card_detailArea}>
+        <div className={styles.card_recruitmentTitle}>3度の飯よりReact！フロントエンドのエキスパートになりたいエンジニア募集！</div>
+
+        <div className={styles.card_avatorArea}>
+          <img src={avator} alt="企業アイコン" />
+          <span>株式会社</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default RecruitmentCard;
