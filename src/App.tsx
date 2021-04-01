ceb325f7b5c34c010411ecc4e005e5a591926b27
yuto.yamamura <yamamu24@gmail.com>
2021-04-01 22:35:44 +0900

    RecruitmentCard 実装

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import "./App.scss";
 import { BrowserRouter, Route } from "react-router-dom";
 import RecruitmentIndexPage from "./scenes/recruitment/RecruitmentIndexPage";
 import SignInPage from "./scenes/signIn";
+import RecruitmentCard from "./components/RecruitmentCard";
+
+const logo01 = "logo01.png";
+const avator01 = "avator01.png";
 
 const App: FC = () => (
   <BrowserRouter>
@@ -10,6 +14,8 @@ const App: FC = () => (
       <Route exact path="/" component={RecruitmentIndexPage} />
       <Route path="/sign_in" component={SignInPage} />
     </div>
+
+    <RecruitmentCard logoFileName={logo01} avatorFileName={avator01}></RecruitmentCard>
   </BrowserRouter>
 );
 
--- src/components/RecruitmentCard/index.tsx
+++ src/components/RecruitmentCard/index.tsx
@@ -1,8 +1,26 @@
 import React from "react";
 import styles from "./style.module.scss";
 
-const RecruitmentCard = () => {
-  return <div className={styles.card}>recruitmentCard</div>;
+import avator01 from "../../images/avator01.png";
+
+const RecruitmentCard = (props: { logoFileName: string | undefined, avatorFileName: string | undefined }) => {
+  var logoSrc = './images/' + props.logoFileName;
+  var avatorSrc = './images/' + props.avatorFileName;
+
+  return (
+    <div className={styles.card}>
+      <img src={logoSrc} />
+
+      <div className={styles.card_detail}>
+        <div className={styles.card_detail_text}>3度の飯よりReact！フロントエンドのエキスパートになりたいエンジニア募集！</div>
+
+        <div className={styles.card_detail_avators}>
+          <img src={avatorSrc} />
+          <span>株式会社</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default RecruitmentCard;
